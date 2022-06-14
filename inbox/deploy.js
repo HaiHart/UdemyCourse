98e99ef12b029fcc77a37a16e58f059900bd574c
HaiHart <hai.hohartfilli@hcmut.edu.vn>
2022-06-14 16:01:16 +0700

    test

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,19 @@
 // deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const {interface,bytecode}=require('./compile')
+
+const provider = new HDWalletProvider('magic ribbon adapt unveil acid fence inmate earn blue distance taxi diet',
+	'https://rinkeby.infura.io/v3/49834aa57ce4481abf60be06b4d934fb'	
+)
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+	const accounts = await web3.eth.getAccounts()
+	console.log('Attempting to deploy from', accounts[0])
+	const rs= await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: bytecode, arguments: ['Hi there'] })
+		.send({ gas: '1000000', from: accounts[0] })
+	console.log('Deployed to ', rs)
+	provider.engine.stop()
+}
