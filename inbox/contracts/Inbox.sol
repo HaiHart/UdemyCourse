8262f1d05c150593016183fa00b66e4f46feb098
HaiHart <hai.hohartfilli@hcmut.edu.vn>
2022-06-14 14:06:15 +0700

    compile

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,4 +1,18 @@
 pragma solidity ^0.4.17;
 // linter warnings (red underline) about pragma version can igonored!
-
+   contract Inbox{
+        string public message;
+        function Inbox(string InitialMessage) public{
+            message=InitialMessage;
+        }
+        function setMessage(string newMessage)public{
+            message = newMessage;
+        }
+        function doMath( int a, int b){
+            a+b;
+            a-b;
+            a*b;
+            a==0;
+        }
+    }
 // contract code will go here
