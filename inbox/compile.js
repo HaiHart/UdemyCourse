393f25d04d128cec8b3088f84110d13935e7d9bd
HaiHart <hai.hohartfilli@hcmut.edu.vn>
2022-06-14 15:19:11 +0700

    test

--- inbox/compile.js
+++ inbox/compile.js
@@ -3,7 +3,8 @@ const path = require('path')
 const fs = require('fs')
 const solc = require('solc')
 
-const inboxPath = path.resolve(__dirname, 'contract', 'inbox.sol')
+const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol')
 const source = fs.readFileSync(inboxPath, 'utf8')
 
-solc.compile(source,1)
+module.exports = solc.compile(source, 1).contracts[':Inbox']
+//console.log(solc.compile(source, 1))
--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,1 +1,49 @@
 // contract test code will go here
+const assert = require('assert')
+const ganache = require('ganache-cli')
+
+const Web3 = require('web3')
+const web3 = new Web3(ganache.provider())
+const {interface,bytecode}=require('../compile')
+
+// let car
+// class Car{
+// 	park() {
+// 		return 'stopped'
+// 	}
+// 	drive() {
+// 		return 'vroom'
+// 	}
+// }
+
+// beforeEach(() => {
+// 	car = new Car()
+// 	console.log('a')
+// })
+
+// describe('Car', () => {
+// 	it('Park function', () => {
+// 		assert.equal(car.park(),'stopped')
+// 	})
+// 	it('Can drive', () => {
+// 		assert.equal(car.drive(),'NOOOO')
+// 	})
+// })
+
+let accounts
+let inbox
+
+beforeEach(async () => {
+	accounts = await web3.eth.getAccounts()
+	inbox=await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: bytecode, arguments: ['Hi there'] })
+		.send({ from: accounts[0], gas: '1000000' })
+})
+
+describe('Inbox', () => {
+	it('deploy', () => {
+		console.log(inbox)
+	})
+
+})
+
