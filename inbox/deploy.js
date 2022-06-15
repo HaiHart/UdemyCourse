ab1e0254d0c0616cae9fac13c581d51c2f1eae90
HaiHart <hai.hohartfilli@hcmut.edu.vn>
2022-06-15 09:25:33 +0700

    change in deploy

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -10,13 +10,13 @@ const provider = new HDWalletProvider('magic ribbon adapt unveil acid fence inma
 const web3 = new Web3(provider)
 
 const deploy = async () => {
-	console.log(Web3)
+	//console.log(Web3)
 	const accounts = await web3.eth.getAccounts()
 	console.log('Attempting to deploy from', accounts[0])
 	const rs= await new web3.eth.Contract(JSON.parse(interface))
 		.deploy({ data: bytecode, arguments: ['Hi there'] })
 		.send({ gas: '1000000', from: accounts[0] })
-	console.log('Deployed to ', rs)
+	console.log('Deployed to ', rs.options.address)
 	provider.engine.stop()
 }
 
