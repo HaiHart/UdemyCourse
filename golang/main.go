8bc395b55b1a0c8fb38ae01d76d2e1cc567242e6
HaiHart <hai.hohartfilli@hcmut.edu.vn>
2022-06-23 11:16:19 +0700

    finished sql get put del post

--- golang/main.go
+++ golang/main.go
@@ -15,35 +15,45 @@ import (
 	// "github.com/joho/godotenv"
 )
 
+type Bird struct {
+	Species     string
+	Description string
+}
 
-
-var connectionString=("root:donQuiote2@tcp(127.0.0.1:3306)/test_api")
+var connectionString = ("root:donQuiote2@tcp(127.0.0.1:3306)/test_api")
 
 var db, err = sql.Open("mysql", connectionString)
 
-type Article struct{
-	Id string `json:"Id"`
-	Title string `json:"Title"`
-	Descript string `json:"desc"`
-	Content string `json:"content"`
+type Article struct {
+	Id       string `json:"Id,omitempty"`
+	Title    string `json:"Title,omitempty"`
+	Descript string `json:"Descript,omitempty"`
+	Content  string `json:"Content,omitempty"`
 }
+
+// type Article struct{
+// 	Id string
+// 	Title string
+// 	Descript string
+// 	Content string
+// }
 
 var Articles []Article
 
-func homePage(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w,"Welcome to the Homepage!")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the Homepage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-func returnAllArticles(w http.ResponseWriter,r *http.Request){
-	results,err:=db.Query("Select * from Articles")
-	if err!=nil{
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	results, err := db.Query("Select * from Articles")
+	if err != nil {
 		panic(err.Error())
 	}
-	for results.Next(){
+	for results.Next() {
 		var article Article
-		err=results.Scan(&article.Id,&article.Title,&article.Descript,&article.Content)
-		if err!=nil{
+		err = results.Scan(&article.Id, &article.Title, &article.Descript, &article.Content)
+		if err != nil {
 			panic(err.Error())
 		}
 		Articles = append(Articles, article)
@@ -52,105 +62,119 @@ func returnAllArticles(w http.ResponseWriter,r *http.Request){
 	json.NewEncoder(w).Encode(Articles)
 }
 
-func returnSingleArticle(w http.ResponseWriter,r *http.Request){
-	vars:=mux.Vars(r)
-	keys:=vars["id"]
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	keys := vars["id"]
 	fmt.Println("hit endpoint: SingleArticle")
-	fmt.Println("Key:"+keys)
-	fmt.Println(fmt.Sprintf( "Select * from Articles where Id= %s",&keys))
-	results,err:=db.Query(fmt.Sprintf( "Select * from Articles where Id= %s",keys))
-	var article  Article
-	if err!=nil{panic(err.Error())}
-	for results.Next(){
-		err=results.Scan(&article.Id,&article.Title,&article.Descript,&article.Content)
-}
-	if err!= nil{panic(err.Error())}
+	fmt.Println("Key:" + keys)
+	fmt.Println(fmt.Sprintf("Select * from Articles where Id= %s", &keys))
+	results, err := db.Query(fmt.Sprintf("Select * from Articles where Id= %s", keys))
+	var article Article
+	if err != nil {
+		panic(err.Error())
+	}
+	for results.Next() {
+		err = results.Scan(&article.Id, &article.Title, &article.Descript, &article.Content)
+	}
+	if err != nil {
+		panic(err.Error())
+	}
 	json.NewEncoder(w).Encode(article)
 }
 
-func createNewArticle(w http.ResponseWriter,r *http.Request){
-	reqBody,_:=ioutil.ReadAll(r.Body)
-	
-	var article Article
-	json.Unmarshal(reqBody,&article)
-	fmt.Println(fmt.Sprintf("insert into Articles values(%s,%s,%s,%s,%s)",article.Id,article.Title,article.Descript,article.Content))
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
 
-	var insert,err=db.Query(fmt.Sprintf("insert into Articles values(%s,%s,%s,%s,%s)",article.Id,article.Title,article.Descript,article.Content))
-	defer insert.Close()
-	if err!=nil{
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	fmt.Fprintf(w, "%+v", string(reqBody))
+	var toStringBody = string(reqBody)
+	var setList Article
+	fmt.Println(toStringBody)
+	if(toStringBody[0]=='\''){
+		reqBody=reqBody[1:len(reqBody)-1]
+	}
+	fmt.Printf("%s\n",string(reqBody))
+	var j_err=json.Unmarshal([]byte(reqBody), &setList)
+	if j_err!=nil{
+		
+		panic(j_err.Error())
+	}
+	fmt.Printf("%+v\n", (setList))
+	fmt.Printf("insert into Articles values(%s,%s,%s,%s)\n", setList.Id, setList.Title, setList.Descript, setList.Content)
+
+	var _,err = db.Exec(fmt.Sprintf("insert into Articles Values(%s,\"%s\",\"%s\",\"%s\");", setList.Id, setList.Title, setList.Descript, setList.Content))
+	// defer insert.Close()
+	if err != nil {
 		panic(err.Error())
 	}
-
-	
-	Articles = append(Articles, article)
 
 	json.NewEncoder(w).Encode(reqBody)
+
 }
 
-func deleteArticle(w http.ResponseWriter,r *http.Request){
-	 vars := mux.Vars(r)
-    // we will need to extract the `id` of the article we
-    // wish to delete
-    id := vars["id"]
-	var delStatement=fmt.Sprintf("delete from Articles where Id= %s ",id)
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	// we will need to extract the `id` of the article we
+	// wish to delete
+	id := vars["id"]
+	var delStatement = fmt.Sprintf("delete from Articles where Id= %s ", id)
 	fmt.Println(delStatement)
-	var del,err=db.Query(delStatement)
+	var del, err = db.Query(delStatement)
 	defer del.Close()
-	if err!=nil{
+	if err != nil {
 		panic(err.Error())
 	}
-    // we then need to loop through all our articles
-    for index, article := range Articles {
-        // if our id path parameter matches one of our
-        // articles
-        if article.Id == id {
-            // updates our Articles array to remove the 
-            // article
-            Articles = append(Articles[:index], Articles[index+1:]...)
-        }
-    }
-}
-
-func updateArticle(w http.ResponseWriter,r *http.Request){
-	vars:=mux.Vars(r)
-	id:=vars["id"]
-	var reqBody,_=ioutil.ReadAll(r.Body)
-	var article Article
-	json.Unmarshal(reqBody,&article)
-	for index, article := range Articles{
-		if(article.Id==id){
-			tmp:=append(Articles[:index],article)
-			Articles=append(tmp,Articles[index+1:]... )
+	// we then need to loop through all our articles
+	for index, article := range Articles {
+		// if our id path parameter matches one of our
+		// articles
+		if article.Id == id {
+			// updates our Articles array to remove the
+			// article
+			Articles = append(Articles[:index], Articles[index+1:]...)
 		}
 	}
 }
 
-func handleRequests(){
+func updateArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var reqBody, _ = ioutil.ReadAll(r.Body)
+	if(string(reqBody)[0]=='\''){
+		reqBody=reqBody[1:len(reqBody)-1]
+	}
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	var upStatement = fmt.Sprintf("update Articles set Descript=\"%s\", Title=\"%s\", Content=\"%s\" where Id= %s ",article.Descript,article.Title,article.Content, id)
+	var _,err=db.Exec(upStatement)
+	if err!=nil{
+		panic(err.Error())
+	}
+}
+
+func handleRequests() {
 	// http.HandleFunc("/",homePage)
 	// http.HandleFunc("/articles/{id}",returnAllArticles)
 	// log.Fatal(http.ListenAndServe(":10000",nil))
 
-	myRouter:=mux.NewRouter().StrictSlash(true)
+	myRouter := mux.NewRouter().StrictSlash(true)
 
-	myRouter.HandleFunc("/",homePage)
-	myRouter.HandleFunc("/articles/",returnAllArticles)
-	myRouter.HandleFunc("/article",createNewArticle).Methods("POST")
-	myRouter.HandleFunc("/articles/{id}",returnSingleArticle)
-	myRouter.HandleFunc("/article/{id}",deleteArticle).Methods("DELETE")
-	myRouter.HandleFunc("/article/{id}",updateArticle).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":10000",myRouter))
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles/", returnAllArticles)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/articles/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{id}", updateArticle).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
-
-func main (){
+func main() {
 	fmt.Println(connectionString)
 
-	if err!=nil{
+	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
-
-	
 
 	handleRequests()
 }
