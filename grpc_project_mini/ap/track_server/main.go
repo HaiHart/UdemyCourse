244cebc2af6e223cb9d8ee787362561fca5def25
HaiHart <hai.hohartfilli@hcmut.edu.vn>
2022-06-24 15:53:25 +0700

    test project

--- grpc_project_mini/ap/track_server/main.go
+++ grpc_project_mini/ap/track_server/main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	. "F:\InternCl\grpc_project_mini\ap\api"
+	. "github.com/HaiHart/UdemyCourse/tree/main/grpc_project_mini/ap/api"
 	"github.com/gorilla/mux"
 )
 
